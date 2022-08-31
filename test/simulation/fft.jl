3f481c0f30f020bbe6eaa578702d65d48d12ad12
Júlio Hoffimann <julio.hoffimann@gmail.com>
2022-08-31 07:50:09 -0300

    Add more tests for FFTGS

--- test/simulation/fft.jl
+++ test/simulation/fft.jl
@@ -1,7 +1,7 @@
 @testset "FFTGS" begin
   # isotropic simulation
   Random.seed!(2019)
-  problem = SimulationProblem(CartesianGrid(100,100), :z=>Float64, 3)
+  problem = SimulationProblem(CartesianGrid(100,100), :z => Float64, 3)
   solver  = FFTGS(:z => (variogram=GaussianVariogram(range=10.),))
   sol     = solve(problem, solver)
 
@@ -11,9 +11,9 @@
 
   # anisotropic simulation
   Random.seed!(2019)
-  problem = SimulationProblem(CartesianGrid(100,100), :z=>Float64, 3)
+  problem = SimulationProblem(CartesianGrid(100,100), :z => Float64, 3)
   solver  = FFTGS(:z => (variogram=GaussianVariogram(MetricBall((20.,5.))),))
-  sol = solve(problem, solver)
+  sol     = solve(problem, solver)
 
   if visualtests
     @test_reference "data/FFT-aniso.png" plot(sol,size=(900,300))
@@ -23,9 +23,23 @@
   Random.seed!(2022)
   grid    = CartesianGrid(100,100)
   vgrid   = view(grid, 1:5000)
-  problem = SimulationProblem(vgrid, :z=>Float64, 3)
+  problem = SimulationProblem(vgrid, :z => Float64, 3)
   solver  = FFTGS(:z => (variogram=GaussianVariogram(range=10.),))
-  sol = solve(problem, solver)
+  sol     = solve(problem, solver)
   @test domain(sol) == vgrid
   @test length(sol[1].z) == 5000
+
+  # conditional simulation
+  Random.seed!(2022)
+  table   = (z=[1.,-1.,1.],)
+  coords  = [(25.,25.), (50.,75.), (75.,50.)]
+  samples = georef(table, coords)
+  sdomain = CartesianGrid(100,100)
+  problem = SimulationProblem(samples, sdomain, :z => Float64, 100)
+  solver  = FFTGS(:z => (variogram=GaussianVariogram(range=10.),))
+  sol     = solve(problem, solver)
+
+  if visualtests
+    @test_reference "data/FFT-cond.png" plot(mean(sol),size=(400,400))
+  end
 end
