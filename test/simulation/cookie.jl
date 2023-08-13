cb0a243e8ac63e646b3b0d08125b27e76eb36113
Júlio Hoffimann <julio.hoffimann@gmail.com>
2023-08-13 09:24:33 -0300

    Refactor tests

--- test/simulation/cookie.jl
+++ test/simulation/cookie.jl
@@ -1,11 +1,11 @@
 @testset "CookieCutter" begin
-  problem = SimulationProblem(CartesianGrid(100, 100), (:facies => Int, :property => Float64), 3)
+  problem = SimulationProblem(CartesianGrid(100, 100), (:f => Int, :z => Float64), 3)
 
-  solver = CookieCutter(DummySimSolver(:facies => NamedTuple()), Dict(0 => DummySimSolver(), 1 => DummySimSolver()))
+  solver = CookieCutter(DummySimSolver(:f => NamedTuple()), Dict(0 => DummySimSolver(), 1 => DummySimSolver()))
 
   @test sprint(show, solver) == "CookieCutter"
   @test sprint(show, MIME"text/plain"(), solver) ==
-        "CookieCutter\n  └─facies ⇨ DummySimSolver\n    └─0 ⇨ DummySimSolver\n    └─1 ⇨ DummySimSolver"
+        "CookieCutter\n  └─f ⇨ DummySimSolver\n    └─0 ⇨ DummySimSolver\n    └─1 ⇨ DummySimSolver"
 
   Random.seed!(1234)
   solution = solve(problem, solver)
